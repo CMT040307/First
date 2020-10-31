4a7e987dce528fc503c608d9eb27a697d6059fb4
CMW040307 <73702970+CMW040307@users.noreply.github.com>
2020-10-31 13:23:06 +0000

    Can now repeat code
    Changed the code so it can now repeat

--- First attempt.py
+++ First attempt.py
@@ -1,17 +1,24 @@
-#name and age
-name = input("What is your name?")
-age = int(input("How old are you?"))
-print("Hello", name)
+def master():
+    #name and age
+    name = input("What is your name?")
+    age = int(input("How old are you?"))
+    print("Hello", name)
 
-gender = input("Are you male or female?")
-if gender.upper() == "MALE":
-    print("I am male too :)")
-else:
-    print("Nice, I am male")
+    gender = input("Are you male or female?")
+    if gender.upper() == "MALE":
+        print("I am male too :)")
+    else:
+        print("Nice, I am male")
 
-fav_food = input("What is your favourite food?")
-if fav_food.upper() == "PIZZA":
-    print("That's my favourite food too :) ")
-else:
-    print("That's a great favourite food! Mine is pizza :) ")
-input("Press enter to exit")
+    fav_food = input("What is your favourite food?")
+    if fav_food.upper() == "PIZZA":
+        print("That's my favourite food too :) ")
+    else:
+        print("That's a great favourite food! Mine is pizza :) ")
+
+    restart = input("Would you like to restart? yes = 1/no = 0")
+    if restart.lower() == "1":
+        master()
+    else:
+        exit()
+master()
