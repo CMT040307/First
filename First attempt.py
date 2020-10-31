58748bc02459b751247142a69d4a0fbf8050be96
CMW040307 <73702970+CMW040307@users.noreply.github.com>
2020-10-31 11:39:49 +0000

    Finished first project
    Quick little first project

---
+++ First attempt.py
@@ -0,0 +1,17 @@
+#name and age
+name = input("What is your name?")
+age = int(input("How old are you?"))
+print("Hello", name)
+
+gender = input("Are you male or female?")
+if gender.upper() == "MALE":
+    print("I am male too :)")
+else:
+    print("Nice, I am male")
+
+fav_food = input("What is your favourite food?")
+if fav_food.upper() == "PIZZA":
+    print("That's my favourite food too :) ")
+else:
+    print("That's a great favourite food! Mine is pizza :) ")
+input("Press enter to exit")
